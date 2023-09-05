a90c4ce936c3876709dd2707d84418ea32ff6d23
bili13310 <dlgusdnr1622@gmail.com>
2023-09-05 23:59:37 +0900

    0905

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/bili/ros_study_ws/src/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
